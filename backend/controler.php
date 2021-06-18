43200656309aee3df1740fba4a7a081d690b0c86
dlowTux <omarlopez22520@gmail.com>
2021-06-18 18:06:29 -0500

    Fix the bug with get the task

--- backend/controler.php
+++ backend/controler.php
@@ -8,11 +8,22 @@ class Controler
         $task->SavaTask($title, $description, $date);
         $this->exito("The task was saved successfully");
     }
-    function GetTasks(){
-        $task= new Task();
-        $result=$task->GetTask();
-        $this->printJSON(["data"=>$result]);
-
+    function GetTasks()
+    {
+        $task = new Task();
+        $result = $task->GetTask();
+          $arreglo=array();
+         $arreglo["tasks"]=array();
+        foreach ($result as $document) {
+                $info=array(
+                    "id"=>$document['_id'],
+                    "title"=>$document['title'],
+                    "description"=>$document['description'],
+                    "date"=>$document['date']
+            );
+            array_push($arreglo);
+        }
+        $this->printJSON(["data" => $result]);
     }
     function error($mensaje)
     {
@@ -26,7 +37,7 @@ class Controler
 
     function printJSON($array)
     {
-      echo json_encode($array);
+        echo json_encode($array);
     }
 }
 
