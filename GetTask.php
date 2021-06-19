757199748795d63ecf94caae0c4523882380dc06
dlowTux <omarlopez22520@gmail.com>
2021-06-18 21:47:26 -0500

    Allow see the contend

--- GetTask.php
+++ GetTask.php
@@ -1,7 +1,9 @@
 <?php
 include_once 'controler.php';
 $task=new Controler();
+header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 echo $task->GetTasks();
 exit();
 ?>
+
